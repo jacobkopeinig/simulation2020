d9eeca74dfe11d183e94f98af4645246b0abdbf6
jacobkopeinig <jacob.kopeinig@gmail.com>
2020-03-12 14:05:45 +0100

    hello world

---
+++ SoftwareEngineering/testPyBullet.py
@@ -0,0 +1,20 @@
+'''
+Created on 5 Mar 2020
+
+@author: jacob
+'''
+import pybullet as p
+import time
+import pybullet_data
+physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
+p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
+p.setGravity(0,0,-10)
+planeId = p.loadURDF("plane.urdf")
+cubeStartPos = [0,0,1]
+cubeStartOrientation = p.getQuaternionFromEuler([0,0,0])
+boxId = p.loadURDF("\\racecar\\racecar_differential.urdf",cubeStartPos, cubeStartOrientation)
+for i in range (10000):
+    p.stepSimulation()
+    time.sleep(1./240.)
+cubePos, cubeOrn = p.getBasePositionAndOrientation(boxId)
+print(cubePos,cubeOrn)
